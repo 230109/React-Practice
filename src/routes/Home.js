976995d3cb386300d60cc94ff10374d55c43a162
HS <hasang2691@naver.com>
2023-08-14 17:57:41 +0900

    third commit

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import Movie from "../components/Movie";
+import styles from "./Home.module.css";
 
 function Home() {
     // loading = data, setLoading = data를 수정할 수 있는 함수
@@ -8,7 +9,7 @@ function Home() {
   const getMovies = async() => {
     const json = await (
       await fetch(
-      `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`
+      `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.5&sort_by=year`
     )).json();
   
     setMovies(json.data.movies);
@@ -18,15 +19,18 @@ function Home() {
     getMovies();
   }, []);
   return (
-  <div>
+    <div className={styles.container}>
     {loading ? (
-    <h1>Loading...</h1> 
+    <div className={styles.loader}>
+        <span>Loading...</span>
+    </div>
     ) : (
-    <div>
+    <div className={styles.movies}>
       {movies.map((movie) => ( 
         <Movie
         key = {movie.id}
         id = {movie.id}
+        year={movie.year}
         coverImg = {movie.medium_cover_image} 
         title = {movie.title} 
         summary = {movie.summary} 
